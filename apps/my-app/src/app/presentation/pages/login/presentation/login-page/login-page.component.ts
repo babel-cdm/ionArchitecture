0e5034ecc5b9acefdb5686935d63edc64b22a896
Delgado Trueba <delgadotrueba@outlook.com>
2022-12-22 15:23:01 +0000

    chore(task-3): :wrench: add environment config

--- apps/my-app/src/app/presentation/pages/login/presentation/login-page/login-page.component.ts
+++ apps/my-app/src/app/presentation/pages/login/presentation/login-page/login-page.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
 
+import { environment } from '@env';
+
 @Component({
   selector: 'ionic-arch-login-page',
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.scss'],
 })
-export class LoginPageComponent {}
+export class LoginPageComponent {
+  environmentName = environment.name
+}
