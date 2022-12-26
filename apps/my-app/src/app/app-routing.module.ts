82f64bf9a87eb21ea551b975256b265432cf6b0c
Delgado Trueba <delgadotrueba@outlook.com>
2022-12-26 21:31:23 +0100

    chore(task-1): :wrench: ionic app-routing

--- apps/my-app/src/app/app-routing.module.ts
+++ apps/my-app/src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import {
+  PreloadAllModules,
+  RouteReuseStrategy,
+  RouterModule,
+  Routes
+} from '@angular/router';
+
+import {
+  IonicRouteStrategy
+} from '@ionic/angular';
+
+const routes: Routes = [
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes, {preloadingStrategy: PreloadAllModules})
+  ],
+  providers: [
+    {provide: RouteReuseStrategy, useClass: IonicRouteStrategy}
+  ],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
