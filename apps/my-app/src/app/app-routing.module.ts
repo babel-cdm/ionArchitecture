20a5b5862ab41ce7fbe4192bf912af4755ba7f46
Delgado Trueba <delgadotrueba@outlook.com>
2022-12-26 21:44:14 +0100

    feat(task-1): :tada: add SCAM Home Component
    * HOW
    * npm i --save-dev @nrwl/angular
    * npx nx generate @nrwl/angular:scam Home --project=my-app --style=sass --change-detection=OnPush --no-export --no-inline-scam --path=apps/my-app/src/app --no-interacti
    
    * Why
    SCAM = Single Component Angular Module
    https://angular-training-guide.rangle.io/modules/module-scam-pattern

--- apps/my-app/src/app/app-routing.module.ts
+++ apps/my-app/src/app/app-routing.module.ts
@@ -11,12 +11,25 @@ import {
   IonicRouteStrategy
 } from '@ionic/angular';
 
+import { HomeComponent } from './home/home.component';
+import { HomeModule } from './home/home.module';
+
 const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
 ];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, {preloadingStrategy: PreloadAllModules})
+    RouterModule.forRoot(routes, {preloadingStrategy: PreloadAllModules}),
+    HomeModule
   ],
   providers: [
     {provide: RouteReuseStrategy, useClass: IonicRouteStrategy}
