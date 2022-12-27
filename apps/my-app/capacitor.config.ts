72615c978d8308e08f13a03c6aeed3d5287bc95b
Delgado Trueba <delgadotrueba@outlook.com>
2022-12-27 15:49:18 +0100

    feat(task-3): ✨ cap sync ios
    * HOW
    npx ionic cap sync ios --project=my-app

--- apps/my-app/capacitor.config.ts
+++ apps/my-app/capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'es.babel.myApp',
   appName: 'my-app',
-  webDir: 'dist/apps/my-app',
+  webDir: '../../dist/apps/my-app',
   bundledWebRuntime: false
 };
 
