c8a6d4e6922bcf49fa2f4dffade4d9a725dfa60d
Delgado Trueba <delgadotrueba@outlook.com>
2023-01-15 08:57:00 +0100

    fix:(task-6) :bug: fix model NativeHttp

--- libs/core/data/plugin/native-http/src/index.ts
+++ libs/core/data/plugin/native-http/src/index.ts
@@ -1,5 +1,10 @@
 import { HttpFactory } from "./lib/factory/HttpServiceFactory";
 import { HttpInterfaceService } from "./lib/HttpInterfaceService"; 
+import { HttpErrorResponse, HttpSuccessResponse } from "./lib/model/response/HttpResponseModel";
 
-export { HttpInterfaceService }; 
+export { 
+    HttpInterfaceService,
+    HttpErrorResponse, 
+    HttpSuccessResponse
+}; 
 export default HttpFactory.getInstance();
