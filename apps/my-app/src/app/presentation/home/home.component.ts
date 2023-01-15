69a1c68dc8bedea56a07e0fabff0f38bf3574107
Delgado Trueba <delgadotrueba@outlook.com>
2023-01-15 09:22:34 +0100

    feat:(task-6)  :sparkles: update view

--- apps/my-app/src/app/presentation/home/home.component.ts
+++ apps/my-app/src/app/presentation/home/home.component.ts
@@ -1,8 +1,8 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
-import { BehaviorSubject, Observable, Subject } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
 
 import { environment } from '@my-app/env';
-import NativeCapHttp from '@core/data/plugin/NativeHttp';
+import NativeCapHttp, { HttpSuccessResponse } from '@core/data/plugin/NativeHttp';
 import sleep from '@core/utils/sleep';
 
 interface Model {
@@ -26,35 +26,10 @@ export class HomeComponent implements OnInit{
   models$: Observable<Model[]> = this.subjectModel.asObservable();
 
   async ngOnInit() {
-    const headers = {
-      'Authorization': 'Basic eyJ1c2VyIjp7ImlkIjoyMTUxNjQsImVtYWlsIjoic3VwcG9ydEBzd2lwZXRhcHNlbGwuY29tIn0sImNvbnRleHQiOiJzdG9yZXMvOWoydHd0Z2QiLCJzdG9yZV9oYXNoIjoiOWoydHd0Z2QiLCJ0aW1lc3RhbXAiOjE0MzI5MDQ1MzAuMDg0NTQ3OH0=.YmI1MjI0M2Q1YWY4MGI5MTg2NDZkOTYwYzBkNTkxZjAzMzc2YTYxYjI2NTc5N2E1NjFlODhlMDE1ZjhjYzlkZA=='
-    }
 
-    // const response = await NativeCapHttp.get({
-    //   endpoint: 'https://private-anon-49753ed7b0-fcctodoapp.apiary-mock.com/todos',
-    //   headers
-    // });
-
-
-    const response = {
-      "status": 200,
-      "statusText": "OK",
-      "data": [
-          {
-              "id": 1,
-              "title": "Finish the Free Code Camp curriculum!",
-              "completed": false
-          },
-          {
-              "id": 2,
-              "title": "Sleep more.",
-              "completed": false
-          }
-      ]
-    }
-
-
-    await sleep(1000);
+    const response = await NativeCapHttp.get<Model[]>({
+      endpoint: 'http://localhost:3001/todos',
+    }) as HttpSuccessResponse<Model[]>;
 
     this.subjectModel.next(response.data);
   }
